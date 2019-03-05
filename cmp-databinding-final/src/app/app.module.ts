d6a1fb0eaeee37970bfa87471b613735ca806b6a
thanos <imran.khan@synesisit.com.bd>
2019-03-05 12:35:02 +0600

    passing data between component

--- cmp-databinding-final/src/app/app.module.ts
+++ cmp-databinding-final/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent // registering component
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule 
+    FormsModule,
+    HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent] // starting component
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,28 @@
-import {Component} from '@angular/core'
+import {Component,OnInit} from '@angular/core'
 @Component({
     selector: 'app-server', // selector can be class or name
-    templateUrl: './server.component.html' //template: `<h1></h1>`
+    templateUrl: './server.component.html', //template: `<h1></h1>`
+    styles:[`
+        .online{
+            color:white;
+        }
+    `]
 })
-export class ServerComponent{
+export class ServerComponent implements OnInit{
     name : string = "imran";
+    allowNewServer=false;
+    serverName:string ="imran ahmed khan";
+    serverStatus=false;
+    constructor(){
+        setTimeout(()=>{
+            this.allowNewServer=true;
+        },2000)
+        this.serverStatus= Math.random() >0.5 ? true:false;
+    }
+    ngOnInit(){}
+    onCreateServer(): void {
+      }
+      getColor() {
+        return this.serverStatus==true? 'Green':'Red'
+    }
 }
